64ef444da7fef06b08b5f0daa86d642204e76d75
Gadea Cabrejas <gadea.cabrejas-saiz@u-paris.fr>
2025-02-27 10:39:52 +0100

    Changes to the plotting function

--- R/fastCNV.R
+++ R/fastCNV.R
@@ -4,21 +4,23 @@
 #' @param sampleName Name of the sample or list of names corresponding to the samples in the `seuratObj`
 #' @param referenceVar The variable name of the annotations in the Seurat metadata
 #' @param referenceLabel The label given to the observations wanted as reference (can be any type of annotation)
+#' @param clustersVar The variable name of the clusters in the Seurat object metadata
 #' @param splitPlotOnVar The name given to the meta.data slot to split the observations on during the `plotCNVResults` step, if different from `referenceVar`.
-#' @param downsizePlot Subset the observations to speed up the plotting process (default = `FALSE`).
 #' @param doPlot If `TRUE` will build a heatmap for each of the samples (default = `TRUE`)
 #' @param printPlot If the heatmap should be printed in the console (default = `FALSE`, the plot will only be in a PDF).
-# @param doRecapPlot Default `TRUE`. Will output the CNV heatmaps by annotation if `TRUE`.
 #' @param prepareCounts If `FALSE` will not run the `prepareCountsForCNVAnalysis` function. Default `TRUE`.
 #' @param pooledReference Default `TRUE`. Will build a pooled reference across all samples if `TRUE`.
 #' @param assay Name of the assay to run the CNV on. Takes the results of `prepareCountsForCNVAnalysis` by default if available
 #' @param getCNVPerChromosomeArm If `TRUE`, will save into the metadata the CNV per chromosome arm
+#' @param getCNVClusters If `TRUE`, will perform clustering on the CNV scores, and save them in the metadata of the seurat object as `cnv_clusters`.
 #' @param savePath Default `.`. If `NULL` the heatmap won't be saved as a pdf.
 #' @param aggregFactor The number of counts per spot desired (default `30 000`). If less than `1000`, will not run the `prepareCountsForCNVAnalysis` function.
 #' @param seuratClusterResolution The resolution wanted for the seurat clusters (default 0.8)
 #' @param aggregateByVar If `referenceVar` is given, whether to use it to pool the observations
 #' @param reClusterSeurat Whether to re-cluster if the Seurat object given already has a `seurat_clusters` slot in its meta.data
 #' @param denoise If the data needs to be denoised (default = `TRUE`)
+#' @param referencePalette The color palette that should be used for `referenceVar`
+#' @param clusters_palette The color palette that should be used for `clustersVar`
 #' @param scaleOnReferenceLabel If you want to scale the results depending on the normal observations
 #' @param thresholdPercentile Which quantiles to take (if 0.01 it will take 0.01-0.99). Background noise appears with higher numbers.
 #' @param geneMetadata List of genes and their metadata (default genes from ensembl version 113)
@@ -33,15 +35,43 @@
 #'
 
 
-fastCNV <- function (seuratObj, sampleName, referenceVar = NULL, referenceLabel = NULL,
-                     splitPlotOnVar = referenceVar, downsizePlot = FALSE, doPlot = TRUE, printPlot = FALSE,
-                     #doRecapPlot = TRUE,
-                     prepareCounts = TRUE, pooledReference = TRUE, assay = NULL,
-                     getCNVPerChromosomeArm = TRUE, savePath = ".", denoise = TRUE,
-                     # mclust_thresholds = FALSE,
-                     aggregFactor=15000, seuratClusterResolution = 0.8, aggregateByVar = T,
-                     reClusterSeurat = F, scaleOnReferenceLabel = TRUE, thresholdPercentile = 0.01,
-                     geneMetadata=getGenes(), windowSize=150, windowStep=10, saveGenomicWindows = FALSE, topNGenes=7000){
+fastCNV <- function (seuratObj,
+                     sampleName,
+                     referenceVar = NULL,
+                     referenceLabel = NULL,
+                     clustersVar = "cnv_clusters",
+                     splitPlotOnVar = clustersVar,
+                     doPlot = TRUE,
+                     printPlot = FALSE,
+                     prepareCounts = TRUE,
+                     pooledReference = TRUE,
+                     assay = NULL,
+                     getCNVPerChromosomeArm = TRUE,
+                     getCNVClusters = TRUE,
+                     savePath = ".",
+                     denoise = TRUE,
+                     referencePalette = as.character(paletteer::paletteer_d("pals::glasbey")),
+                     clusters_palette = scales::hue_pal(),
+                     aggregFactor=15000,
+                     seuratClusterResolution = 0.8,
+                     aggregateByVar = T,
+                     reClusterSeurat = F,
+                     scaleOnReferenceLabel = TRUE,
+                     thresholdPercentile = 0.01,
+                     geneMetadata=getGenes(),
+                     windowSize=150,
+                     windowStep=10,
+                     saveGenomicWindows = FALSE,
+                     topNGenes=7000){
+
+  if (getCNVClusters == FALSE) {
+    if (!is.null(clustersVar) && clustersVar == "cnv_clusters") {
+      if(splitPlotOnVar == clustersVar) {
+        splitPlotOnVar = referenceVar
+      }
+      clustersVar = NULL
+    }
+  }
 
   if(!length(seuratObj)==length(sampleName)) stop("error-fastCNV : seuratObj & sampleName should have the same length")
 
@@ -78,16 +108,6 @@ fastCNV <- function (seuratObj, sampleName, referenceVar = NULL, referenceLabel 
                               windowSize = windowSize, windowStep = windowStep, saveGenomicWindows = saveGenomicWindows, topNGenes = topNGenes)
     invisible(gc())
     message("CNVAnalysis done!")
-    if (doPlot == TRUE) {
-      message("Plotting CNV results. This step may take some time.")
-      for (i in 1:length(seuratObj)) {
-        if (Seurat::Project(seuratObj[[i]]) == "SeuratProject") {Seurat::Project(seuratObj[[i]]) = paste0("Sample",i)}
-          plotCNVResults(seuratObj[[i]], referenceVar = referenceVar, splitPlotOnVar = splitPlotOnVar,
-                         savePath = savePath, printPlot = printPlot, downsizePlot = downsizePlot)
-          invisible(gc())
-      }
-    }
-
   } else {
     message("Running CNVAnalysis")
     seuratObj <- CNVanalysis(seuratObj[[1]], referenceVar = referenceVar, referenceLabel = referenceLabel,
@@ -98,12 +118,6 @@ fastCNV <- function (seuratObj, sampleName, referenceVar = NULL, referenceLabel 
                          windowSize = windowSize, windowStep = windowStep, saveGenomicWindows = saveGenomicWindows, topNGenes = topNGenes)
     invisible(gc())
     message("CNVAnalysis done!")
-    if (doPlot == TRUE) {
-      message("Plotting CNV results. This step may take some time.")
-      plotCNVResults(seuratObj = seuratObj, referenceVar = referenceVar, splitPlotOnVar = splitPlotOnVar,
-                     savePath = savePath, printPlot = printPlot,  downsizePlot = downsizePlot)
-      invisible(gc())
-    }
   }
 
   if (getCNVPerChromosomeArm == TRUE) {
@@ -116,6 +130,37 @@ fastCNV <- function (seuratObj, sampleName, referenceVar = NULL, referenceLabel 
     }
     invisible(gc())
   }
+
+  if (getCNVClusters == TRUE) {
+    if (length(seuratObj) == 1) {
+      seuratObj <- CNVcluster(seuratObj)
+    } else {
+      for (i in 1:length(seuratObj)) {
+        seuratObj[[i]] <- CNVcluster(seuratObj[[i]])
+      }
+    }
+    invisible(gc())
+  }
+
+  if (doPlot == TRUE) {
+    message("Plotting CNV results. This step may take some time.")
+    if (length(seuratObj) > 1) {
+      for (i in 1:length(seuratObj)) {
+        if (Seurat::Project(seuratObj[[i]]) == "SeuratProject") {Seurat::Project(seuratObj[[i]]) = paste0("Sample",i)}
+        if ("cnv_clusters" %in% names(seuratObj[[i]]@meta.data)) {splitPlotOnVar = "cnv_clusters"}
+        plotCNVResults(seuratObj[[i]], referenceVar = referenceVar, splitPlotOnVar = splitPlotOnVar,
+                       savePath = savePath, printPlot = printPlot, referencePalette = referencePalette, clusters_palette = clusters_palette)
+        invisible(gc())
+      }
+    } else {
+      if ("cnv_clusters" %in% names(seuratObj@meta.data)) {splitPlotOnVar = "cnv_clusters"}
+      plotCNVResults(seuratObj = seuratObj, referenceVar = referenceVar, splitPlotOnVar = splitPlotOnVar, clustersVar = clustersVar,
+                     savePath = savePath, printPlot = printPlot, referencePalette = referencePalette, clusters_palette = clusters_palette)
+      invisible(gc())
+    }
+  }
+
+
 
   return (seuratObj)
 }
--- R/plotCNVResults.R
+++ R/plotCNVResults.R
@@ -1,11 +1,13 @@
 #' A function to plot the CNV results into a heatmap
 #'
 #' @param seuratObj Seurat object containing the data used to get the genomicScores.
-#' @param referenceVar The variable name of the annotations in the Seurat metadata
+#' @param referenceVar The variable name of the annotations in the Seurat object metadata
+#' @param clustersVar The variable name of the clusters in the Seurat object metadata
 #' @param splitPlotOnVar The variable name on which to split the heatmap rows.
 #' @param savePath Path to save the pdf heatmap. If `NULL`, plot won't be saved (default = `.`).
 #' @param printPlot If the heatmap should be printed in the console.
-#' @param downsizePlot Subset the observations to speed up the plotting process (default = `FALSE`).
+#' @param referencePalette The color palette that should be used for `referenceVar`
+#' @param clusters_palette The color palette that should be used for `clustersVar`
 #'
 #' @import ComplexHeatmap
 #' @import Seurat
@@ -17,205 +19,122 @@
 #' @import grid
 #' @import paletteer
 #' @import magick
+#' @import scales
 #'
 #' @return This function builds a heatmap and saves it as a .pdf file in your working directory
 #'
 #' @export
 #'
 
-plotCNVResults <- function(seuratObj, referenceVar = NULL,
-                           splitPlotOnVar = referenceVar, savePath = ".",
-                           printPlot = FALSE, downsizePlot = FALSE){
+plotCNVResults <- function(seuratObj, referenceVar = NULL, clustersVar = "cnv_clusters",
+                           splitPlotOnVar = clustersVar, savePath = ".",
+                           printPlot = FALSE, referencePalette = as.character(paletteer::paletteer_d("pals::glasbey")),
+                           clusters_palette = scales::hue_pal()){
+
   M <- t(as.matrix(Seurat::GetAssay(seuratObj, "genomicScores")["data"]))
-  if (downsizePlot == TRUE && !is.null(referenceVar)) {
-    # K-means clustering
-    LClust <- kmeans(M, centers = 40)
-    LClust <- split(Seurat::Cells(seuratObj), LClust$cluster)
-    LClust <- lapply(LClust, function(x) split(x, Seurat::FetchData(seuratObj, vars = referenceVar, cells = x)))
 
-    centroid_list <- list()
-    annotation_list <- list()
-    split_list <- list()
-
-    for (cluster in names(LClust)) {
-      for (annotation in names(LClust[[cluster]])) {
-        rows <- intersect(rownames(M), LClust[[cluster]][[annotation]])
-        if (length(rows) > 0) {
-          subset_matrix <- M[rows, , drop=FALSE]
-          if (nrow(subset_matrix) > 1) {
-            centroid <- colMeans(subset_matrix)
-            centroid_list[[paste0(cluster, "_", annotation)]] <- centroid
-            annotation_list[[paste0(cluster, "_", annotation)]] <- annotation
-            if (!is.null(splitPlotOnVar)) {
-              split_values <- seuratObj@meta.data[LClust[[cluster]][[annotation]], splitPlotOnVar, drop = TRUE]
-              split_list[[paste0(cluster, "_", annotation)]] <- names(sort(table(split_values), decreasing = TRUE))[1]  # Take the most common value
-            }
-          } else if (nrow(subset_matrix) == 1) {
-            centroid_list[[paste0(cluster, "_", annotation)]] <- subset_matrix
-            annotation_list[[paste0(cluster, "_", annotation)]] <- annotation
-            if (!is.null(splitPlotOnVar)) {
-              split_list[[paste0(cluster, "_", annotation)]] <- seuratObj@meta.data[rownames(subset_matrix), splitPlotOnVar]
-            }
-          }
-        }
-      }
-    }
-    annotation_df <- data.frame(Annotation = unlist(annotation_list))
-    M <- as.matrix(do.call(rbind, centroid_list))
-    rownames(M) <- names(centroid_list)
-    if (!is.null(splitPlotOnVar)) {
-      split_df <- data.frame(Split = unlist(split_list))
-    } else {
-      split_df <- NULL
-    }
-  } else if (downsizePlot == FALSE && !is.null(referenceVar)) {
+  if (!is.null(referenceVar)) {
     annotation_df <- as.data.frame(seuratObj@meta.data[[referenceVar]])
     colnames(annotation_df) <- "Annotations"
+    annot_colors <- setNames(referencePalette[1:length(unique(annotation_df$Annotations))], unique(annotation_df$Annotations))
     if (!is.null(splitPlotOnVar)) {
       split_df <- as.data.frame(Seurat::FetchData(seuratObj, vars = splitPlotOnVar))
       colnames(split_df) <- "Split"
     } else {
       split_df <- NULL
     }
-  } else if (downsizePlot == TRUE && is.null(referenceVar)) {
-    LClust <- kmeans(M, centers = 150)
-    LClust <- split(Seurat::Cells(seuratObj), LClust$cluster)
-    centroid_list <- list()
-    split_list <- list()
-    for (cluster in names(LClust)) {
-      cluster_rows <- unlist(LClust[[cluster]])
-      rows <- intersect(rownames(M), cluster_rows)
-      if (length(rows) > 0) {
-        subset_matrix <- M[rows, , drop = FALSE]
-        if (nrow(subset_matrix) > 1) {
-          centroid <- colMeans(subset_matrix)
-          centroid_list[[cluster]] <- centroid
-          if (!is.null(splitPlotOnVar)) {
-            split_values <- seuratObj@meta.data[cluster_rows, splitPlotOnVar, drop = TRUE]
-            split_list[[cluster]] <- names(sort(table(split_values), decreasing = TRUE))[1]  # Take the most common value
-          }
-        } else if (nrow(subset_matrix) == 1) {
-          centroid_list[[cluster]] <- as.numeric(subset_matrix)
-          if (!is.null(splitPlotOnVar)) {
-            split_list[[cluster]] <- seuratObj@meta.data[rownames(subset_matrix), splitPlotOnVar]
-          }
-        }
-      }
-    }
-    M <- as.matrix(do.call(rbind, centroid_list))
-    if (!is.null(splitPlotOnVar)) {
-      split_df <- data.frame(Split = unlist(split_list))
-    } else {
-      split_df <- NULL
-    }
-  } else if (downsizePlot == FALSE && is.null(splitPlotOnVar)) {
+  } else if (is.null(splitPlotOnVar)) {
     split_df <- NULL
   }
-
-  if(is.null(referenceVar)) {
-    if (is.null(splitPlotOnVar)) {splitPlot = NULL}else{splitPlot = as.factor(split_df[[1]])}
-    hm <-  ComplexHeatmap::Heatmap(
-      M,
-      border = TRUE,
-      cluster_columns = FALSE,
-      show_row_names = FALSE,
-      show_column_names = FALSE,
-      use_raster = TRUE,
-      clustering_distance_rows = "euclidean",
-      clustering_method_rows = "ward.D",
-      column_split = as.numeric(sapply(strsplit(rownames(as.matrix(Seurat::GetAssay(seuratObj, assay = "genomicScores")["data"])), ".", fixed = TRUE), function(z) z[1])),
-      column_title_gp = grid::gpar(fontsize = 8),
-      column_title = c(1:22,"X"),
-      row_split = splitPlot,
-      col = circlize::colorRamp2(c(-1, -0.6, -0.3, 0, 0.3, 0.6, 1), c("#0B2F7EFF", "#2A4D9EFF", "#A0A0FFFF", "white", "#E3807D", "#A4161A","#7A0A0D")),
-      heatmap_legend_param = list(
-        title = "CNV",
-        title_gp = grid::gpar(fontsize = 11),  # Font size for the title
-        labels_gp = grid::gpar(fontsize = 8),  # Font size for the labels
-        grid_height = grid::unit(1, "cm"),  # Size of the grid cells in the legend
-        grid_width = grid::unit(0.6, "cm"))
-    )
-  } else {
-    if (referenceVar == splitPlotOnVar) {
-      rown = NULL
-    } else {
-      rown = character(0)
-    }
-    palette <- as.character(paletteer::paletteer_d("pals::glasbey"))
-    if (length(unique(annotation_df$Annotation)) > length(palette)) {
-      hm <-  ComplexHeatmap::Heatmap(
-        M,
-        right_annotation = ComplexHeatmap::rowAnnotation(
-          Annotations = annotation_df$Annotations,
-          annotation_legend_param = list(
-            title = "Annotations",  # Set the title of the annotation legend
-            title_gp = grid::gpar(fontsize = 11),  # Font size for the title
-            labels_gp = grid::gpar(fontsize = 8),  # Font size for the labels
-            legend_height = grid::unit(3, "cm"),  # Height of the annotation legend
-            legend_width = grid::unit(1.5, "cm"),  # Width of the annotation legend
-            grid_height = grid::unit(0.6, "cm"),  # Size of the grid cells in the legend
-            grid_width = grid::unit(0.6, "cm")  # Size of the grid cells in the legend
-          )
-        ),
-        border = TRUE,
-        cluster_columns = FALSE,
-        show_row_names = FALSE,
-        show_column_names = FALSE,
-        use_raster = TRUE,
-        clustering_distance_rows = "euclidean",
-        clustering_method_rows = "ward.D",
-        column_split = as.numeric(sapply(strsplit(rownames(as.matrix(Seurat::GetAssay(seuratObj, assay = "genomicScores")["data"])), ".", fixed = TRUE), function(z) z[1])),
-        column_title_gp = grid::gpar(fontsize = 8),
-        column_title = c(1:22,"X"),
-        row_split = as.factor(split_df[[1]]),
-        row_title = rown,
-        col = circlize::colorRamp2(c(-1, -0.6, -0.3, 0, 0.3, 0.6, 1), c("#0B2F7EFF", "#2A4D9EFF", "#A0A0FFFF", "white", "#E3807D", "#A4161A","#7A0A0D")),
-        heatmap_legend_param = list(
-          title = "CNV",
-          title_gp = grid::gpar(fontsize = 11),  # Font size for the title
-          labels_gp = grid::gpar(fontsize = 8),  # Font size for the labels
-          grid_height = grid::unit(1, "cm"),  # Size of the grid cells in the legend
-          grid_width = grid::unit(0.6, "cm"))
-      )
-    } else {
-      annot_colors <- setNames(palette[1:length(unique(annotation_df$Annotation))], unique(annotation_df$Annotation))
-      hm <-  ComplexHeatmap::Heatmap(
-        M,
-        right_annotation = ComplexHeatmap::rowAnnotation(
-          Annotations = annotation_df$Annotations,
-          col = list(Annotations = annot_colors),
-          annotation_legend_param = list(
-            title = "Annotations",  # Set the title of the annotation legend
-            title_gp = grid::gpar(fontsize = 11),  # Font size for the title
-            labels_gp = grid::gpar(fontsize = 8),  # Font size for the labels
-            legend_height = grid::unit(3, "cm"),  # Height of the annotation legend
-            legend_width = grid::unit(1.5, "cm"),  # Width of the annotation legend
-            grid_height = grid::unit(0.6, "cm"),  # Size of the grid cells in the legend
-            grid_width = grid::unit(0.6, "cm")  # Size of the grid cells in the legend
-          )
-        ),
-        border = TRUE,
-        cluster_columns = FALSE,
-        show_row_names = FALSE,
-        show_column_names = FALSE,
-        use_raster = TRUE,
-        clustering_distance_rows = "euclidean",
-        clustering_method_rows = "ward.D",
-        column_split = as.numeric(sapply(strsplit(rownames(as.matrix(Seurat::GetAssay(seuratObj, assay = "genomicScores")["data"])), ".", fixed = TRUE), function(z) z[1])),
-        column_title_gp = grid::gpar(fontsize = 8),
-        column_title = c(1:22,"X"),
-        row_split = as.factor(split_df[[1]]),
-        row_title = rown,
-        col = circlize::colorRamp2(c(-1, -0.6, -0.3, 0, 0.3, 0.6, 1), c("#0B2F7EFF", "#2A4D9EFF", "#A0A0FFFF", "white", "#E3807D", "#A4161A","#7A0A0D")),
-        heatmap_legend_param = list(
-          title = "CNV",
-          title_gp = grid::gpar(fontsize = 11),  # Font size for the title
-          labels_gp = grid::gpar(fontsize = 8),  # Font size for the labels
-          grid_height = grid::unit(1, "cm"),  # Size of the grid cells in the legend
-          grid_width = grid::unit(0.6, "cm"))
-      )
-    }
+  if (!is.null(clustersVar)) {
+    clusters_df <- as.data.frame(seuratObj@meta.data[[clustersVar]])
+    colnames(clusters_df) <- "Clusters"
+    clusters_colors <- setNames(clusters_palette(length(unique(clusters_df$Clusters))), unique(clusters_df$Clusters))
   }
+
+  if (!is.null(referenceVar) && is.null(clustersVar)) {
+    annotation_heatmap <- ComplexHeatmap::rowAnnotation(
+      Annotations = annotation_df$Annotations,
+      col = list(Annotations = annot_colors),
+      annotation_legend_param = list(
+        title = "Annotations",
+        title_gp = grid::gpar(fontsize = 11),
+        labels_gp = grid::gpar(fontsize = 8),
+        legend_height = grid::unit(3, "cm"),
+        legend_width = grid::unit(1.5, "cm"),
+        grid_height = grid::unit(0.6, "cm"),
+        grid_width = grid::unit(0.6, "cm")
+      )
+    )
+  }
+
+  if (is.null(referenceVar) && !is.null(clustersVar)) {
+    annotation_heatmap <- ComplexHeatmap::rowAnnotation(
+      Clusters = clusters_df$Clusters,
+      col = clusters_colors,
+      annotation_legend_param = list(
+        title = "Clusters",
+        title_gp = grid::gpar(fontsize = 11),
+        labels_gp = grid::gpar(fontsize = 8),
+        legend_height = grid::unit(3, "cm"),
+        legend_width = grid::unit(1.5, "cm"),
+        grid_height = grid::unit(0.6, "cm"),
+        grid_width = grid::unit(0.6, "cm")
+      )
+    )
+  }
+
+  if (is.null(referenceVar) && is.null(clustersVar)) {
+    annotation_heatmap <- NULL
+  }
+
+  if (!is.null(referenceVar) && !is.null(clustersVar)) {
+    annotation_heatmap <- ComplexHeatmap::rowAnnotation(
+      Annotations = annotation_df$Annotations,
+      Clusters = clusters_df$Clusters,
+      col = list(
+        Annotations = annot_colors,
+        Clusters = clusters_colors
+      ),
+      annotation_legend_param = list(
+        Annotations = list(
+          title = "Annotations",
+          title_gp = grid::gpar(fontsize = 11),
+          labels_gp = grid::gpar(fontsize = 8)
+        ),
+        Clusters = list(
+          title = "CNV cluster",
+          title_gp = grid::gpar(fontsize = 11),
+          labels_gp = grid::gpar(fontsize = 8)
+        )
+      )
+    )
+  }
+
+  hm <-  ComplexHeatmap::Heatmap(
+    M,
+    right_annotation = annotation_heatmap,
+    border = TRUE,
+    cluster_columns = FALSE,
+    show_row_names = FALSE,
+    show_column_names = FALSE,
+    use_raster = TRUE,
+    clustering_distance_rows = "euclidean",
+    clustering_method_rows = "ward.D",
+    column_split = as.numeric(sapply(strsplit(rownames(as.matrix(Seurat::GetAssay(seuratObj, assay = "genomicScores")["data"])), ".", fixed = TRUE), function(z) z[1])),
+    column_title_gp = grid::gpar(fontsize = 8),
+    column_title = c(1:22,"X"),
+    row_split = as.factor(split_df[[1]]),
+    row_title = NULL,
+    col = circlize::colorRamp2(c(-1, -0.6, -0.3, 0, 0.3, 0.6, 1), c("#0B2F7EFF", "#2A4D9EFF", "#A0A0FFFF", "white", "#E3807D", "#A4161A","#7A0A0D")),
+    heatmap_legend_param = list(
+      title = "CNV",
+      title_gp = grid::gpar(fontsize = 11),
+      labels_gp = grid::gpar(fontsize = 8),
+      grid_height = grid::unit(1, "cm"),
+      grid_width = grid::unit(0.6, "cm"))
+    )
+
   if(printPlot == TRUE) {
     grid::grid.newpage()
     grid::pushViewport(grid::viewport(layout = grid::grid.layout(nrow = 2, heights = grid::unit.c(grid::unit(1, "cm"), grid::unit(1, "null")))))
@@ -228,7 +147,7 @@ plotCNVResults <- function(seuratObj, referenceVar = NULL,
   }
 
   if(!is.null(savePath)) {
-    fname <- file.path(savePath, paste0("heatmap.fastCNV_", splitPlotOnVar,"_",seuratObj@project.name,".pdf"))
+    fname <- file.path(savePath, paste0("heatmap.fastCNV_",seuratObj@project.name,".pdf"))
     pdf(width = 12, height = 7, file = fname)
     grid::grid.newpage()
     grid::pushViewport(grid::viewport(layout = grid::grid.layout(nrow = 2, heights = grid::unit.c(grid::unit(1, "cm"),grid::unit(1, "null")))))
